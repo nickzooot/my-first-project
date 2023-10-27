123e8bf49a615a4784f40f82c8193f3a746f2a17
Nick <paninnikita83@gmail.com>
2023-10-27 17:04:25 +0300

    Add train.py

--- src/mylib/train.py
+++ src/mylib/train.py
@@ -1,132 +1,390 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-'''
-The :mod:`mylib.train` contains classes:
 
-- :class:`mylib.train.Trainer`
 
-The :mod:`mylib.train` contains functions:
+!pip install pyproject.toml-based projects
 
-- :func:`mylib.train.cv_parameters`
-'''
-from __future__ import print_function
+from google.colab import drive
+drive.mount('/content/drive')
 
-__docformat__ = 'restructuredtext'
+# Commented out IPython magic to ensure Python compatibility.
+# %cd /content/drive/MyDrive/Colab\ Notebooks/Emotion-Detection-From-Tweets-Using-BERT-and-SVM-Ensemble-Model-master
 
-import numpy
-from scipy.special import expit
-from sklearn.linear_model import LogisticRegression
+!pip install -qq sentencepiece
+!pip install -qq transformers
+!pip install -qq unidecode
+
+!gdown --id -qq 1up7d8aWd0H-QDN2konB_4oAwa4oHJQr2
+!gdown --id -qq 141nsJ-dt-ZDR0aM1GkY2CGEmKeIM7O1F
+!gdown --id 1mUoiYzyUs6ihlmkqskdPPgo0O1ainWqw
+!unzip -qq wassa_dataset.zip -y
+!rm wassa_dataset.zip
+
+# Commented out IPython magic to ensure Python compatibility.
+import transformers
+from transformers import BertModel, BertTokenizer, AdamW, get_linear_schedule_with_warmup
+import torch
+import torch.utils
+from torch import nn, optim
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+import sklearn
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.preprocessing import LabelEncoder
+import pickle
+from collections import defaultdict
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import itertools
+from nltk.tokenize import TweetTokenizer
+from sklearn.metrics import accuracy_score
+import re
+import unidecode
+from nltk import PorterStemmer, word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import precision_score, recall_score, f1_score, classification_report
+from transformers import MT5Tokenizer
+from transformers import RobertaTokenizer, RobertaModel, BertTokenizer, BertModel, AlbertModel, AlbertTokenizer, AutoModel, AutoTokenizer
+import pandas as pd
+# %config InlineBackend.figure_format = 'svg'
+# %matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-class SyntheticBernuliDataset(object):
-    r'''Base class for synthetic dataset.'''
-    def __init__(self, n=10, m=100, seed=42):
-        r'''Constructor method
+"""#Preprocessing the dataset"""
 
-        :param n: the number of feature
-        :type n: int
-        :param m: the number of object
-        :type m: int
-        :param seed: seed for random state.
-        :type seed: int
-        '''
-        rs = numpy.random.RandomState(seed)
-
-        self.w = rs.randn(n) # Генерим вектор параметров из нормального распределения
-        self.X = rs.randn(m, n) # Генерим вектора признаков из нормального распределения
-
-        self.y = rs.binomial(1, expit(self.X@self.w)) # Гипотеза порождения данных - целевая переменная из схемы Бернули
-
-
-class Trainer(object):
-    r'''Base class for all trainer.'''
-    def __init__(self, model, X, Y, seed=42):
-        r'''Constructor method
-
-        :param model: The class with fit and predict methods.
-        :type model: object
-
-        :param X: The array of shape 
-            `num_elements` :math:`\times` `num_feature`.
-        :type X: numpy.array
-        :param Y: The array of shape 
-            `num_elements` :math:`\times` `num_answers`.
-        :type Y: numpy.array
-
-        :param seed: Seed for random state.
-        :type seed: int
-        '''
-        self.model = model
-        self.seed = seed
-        (
-            self.X_train, 
-            self.X_val, 
-            self.Y_train, 
-            self.Y_val
-        ) = train_test_split(X, Y, random_state=self.seed)
-
-    def train(self):
-        r''' Train model
-        '''
-        self.model.fit(self.X_train, self.Y_train)
-
-    def eval(self, output_dict=False):
-        r'''Evaluate model for initial validadtion dataset.
-        '''
-        return classification_report(
-            self.Y_val, 
-            self.model.predict(
-                self.X_val), output_dict=output_dict)
-
-    def test(self, X, Y, output_dict=False):
-        r"""Evaluate model for given dataset.
-        
-        :param X: The array of shape 
-            `num_elements` :math:`\times` `num_feature`.
-        :type X: numpy.array
-        :param Y: The array of shape 
-            `num_elements` :math:`\times` `num_answers`.
-        :type Y: numpy.array
-        """
-        return classification_report(
-            Y, self.model.predict(X), output_dict=output_dict)
+def preprocess_text(text_list):
+  for i, text in enumerate(text_list):
+    # eliminate URLs
+    result = re.result = re.sub(r"http\S+", "", text)
+    # eliminate email addresses
+    result = re.sub('\S*@\S*\s?', '', result)
+    text_list[i] = result
 
 
-def cv_parameters(X, Y, seed=42, minimal=0.1, maximum=25, count=100):
-    r'''Function for the experiment with different regularisation parameters 
-        and return accuracy and weidth for LogisticRegression for each parameter.
 
-    :param X: The array of shape 
-        `num_elements` :math:`\times` `num_feature`.
-    :type X: numpy.array
-    :param Y: The array of shape 
-        `num_elements` :math:`\times` `num_answers`.
-    :type Y: numpy.array
+"""# Useful functions"""
 
-    :param seed: Seed for random state.
-    :type seed: int
-    :param minimal: Minimum value for the Cs linspace.
-    :type minimal: int
-    :param maximum: Maximum value for the Cs linspace.
-    :type maximum: int
-    :param count: Number of the Cs points.
-    :type count: int
-    '''
+import pandas as pd
 
-    Cs = numpy.linspace(minimal, maximum, count)
-    parameters = []
-    accuracy = []
-    for C in Cs:
-        trainer = Trainer(
-            LogisticRegression(penalty='l1', solver='saga', C=1/C),
-            X, Y,
-        )
+df = pd.DataFrame(columns=['acc', 'precision'])
 
-        trainer.train()
+def pickle_object(object, location):
+    filehandler = open(location, "wb")
+    pickle.dump(object, filehandler)
+    filehandler.close()
 
-        accuracy.append(trainer.eval(output_dict=True)['accuracy'])
-        
-        parameters.extend(trainer.model.coef_)
+def pickle_load_object(location):
+    file = open(location, 'rb')
+    object = pickle.load(file)
+    file.close()
+    return object
 
-    return Cs, accuracy, parameters
+def plot_confusion_matrix(cm,
+                          target_names,
+                          title='Confusion matrix',
+                          cmap=None,
+                          normalize=True):
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+    cmap='Purples'
+    if cmap is None:
+        cmap = plt.get_cmap('Purples')
+
+    plt.rcParams.update({'font.size': 20})
+    plt.figure(figsize=(12, 8))
+    im = plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    im = plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    plt.show()
+
+def compute_metrics(y_true, predictions, name):
+  average = "macro"
+  acc = accuracy_score(y_true, predictions)
+  precision = precision_score(y_true, predictions, average=average)
+  recall = recall_score(y_true, predictions, average=average)
+  f1 = f1_score(y_true, predictions, average=average)
+  df = pd.DataFrame(columns=['accuracy', 'precision', 'recall', 'f1_score'])
+  df.loc[len(df.index)] = [acc, precision, recall, f1]
+  df.index = [name]
+  return df
+
+"""# Model and tokenizer selection functions
+
+
+
+
+"""
+
+def get_model(model_name):
+  model = None
+  if model_name == 'bert-base-cased':
+      model = BertModel.from_pretrained(model_name)
+  if model_name == 'bert-large-cased':
+      model = BertModel.from_pretrained(model_name)
+  if model_name == 'roberta-base':
+      model = RobertaModel.from_pretrained(model_name)
+  if model_name == 'roberta-large':
+      model = RobertaModel.from_pretrained(model_name)
+
+  if model_name == 'vinai/bertweet-base':
+      model = AutoModel.from_pretrained(model_name)
+  if model_name == 'vinai/bertweet-large':
+      model = AutoModel.from_pretrained(model_name)
+
+  return model
+
+def get_tokenizer(model_name):
+    tokenizer = None
+    if model_name == 'bert-base-cased':
+      tokenizer = BertTokenizer.from_pretrained(model_name)
+    if model_name == 'bert-large-cased':
+      tokenizer = BertTokenizer.from_pretrained(model_name)
+
+    if model_name == 'roberta-base':
+      tokenizer = RobertaTokenizer.from_pretrained(model_name)
+    if model_name == 'roberta-large':
+      tokenizer = RobertaTokenizer.from_pretrained(model_name)
+
+    if model_name == 'vinai/bertweet-base':
+      tokenizer = AutoTokenizer.from_pretrained(model_name, normalization=True, use_fast=False)
+    if model_name == 'vinai/bertweet-large':
+      tokenizer = AutoTokenizer.from_pretrained(model_name, normalization=True, use_fast=False)
+
+    return tokenizer
+
+"""# Choosing the device and model"""
+
+!pip3 install emoji==0.6.0
+
+
+
+"""# Building the PyTorch datasets and dataloaders"""
+
+class WassaDataset(Dataset):
+
+  def __init__(self, texts, emotions, tokenizer, max_len):
+    self.texts = texts
+    self.emotions = emotions
+    self.tokenizer = tokenizer
+    self.max_len = max_len
+
+  def __len__(self):
+    return len(self.texts)
+
+  def __getitem__(self, item):
+    text = str(self.texts[item])
+    emotion = self.emotions[item]
+    encoding = self.tokenizer.encode_plus(
+      text,
+      add_special_tokens=True,
+      max_length=self.max_len,
+      return_token_type_ids=False,
+      padding='max_length',
+      return_attention_mask=True,
+      return_tensors='pt',
+    )
+
+    return {
+      'text': text,
+      'input_ids': encoding['input_ids'].flatten(),
+      'attention_mask': encoding['attention_mask'].flatten(),
+      'emotions': torch.tensor(emotion, dtype=torch.long)
+    }
+
+def create_data_loader(texts, emotions, tokenizer, max_len, batch_size):
+  dataset = WassaDataset(
+    texts=texts,
+    emotions=emotions,
+    tokenizer=tokenizer,
+    max_len=max_len)
+
+  return DataLoader(
+    dataset,
+    batch_size=batch_size,
+    num_workers=2
+  )
+
+
+
+"""# Building the model"""
+
+class EmotionsClassifier(nn.Module):
+  def __init__(self, n_classes, model_name):
+    super(EmotionsClassifier, self).__init__()
+    self.bert = get_model(model_name)
+    self.drop = nn.Dropout(p=0.3)
+    self.out = nn.Linear(self.bert.config.hidden_size, n_classes)
+    self.softmax = nn.LogSoftmax(dim=1)
+
+  def forward(self, input_ids, attention_mask):
+    _, pooled_output = self.bert(
+      input_ids=input_ids,
+      attention_mask=attention_mask,
+      return_dict=False
+    )
+    output = self.drop(pooled_output)
+    output = self.out(output)
+    return self.softmax(output)
+
+
+
+"""# Training and validation"""
+
+
+def train_epoch(model, data_loader, loss_fn, optimizer, device, scheduler, n_examples):
+  model = model.train()
+  losses = []
+  correct_predictions = 0
+  for d in tqdm(data_loader):
+    input_ids = d["input_ids"].to(device)
+    attention_mask = d["attention_mask"].to(device)
+    emotions = d["emotions"].to(device)
+    outputs = model(
+      input_ids=input_ids,
+      attention_mask=attention_mask
+    )
+    _, preds = torch.max(outputs, dim=1)
+    loss = loss_fn(outputs, emotions)
+    correct_predictions += torch.sum(preds == emotions)
+    losses.append(loss.item())
+    loss.backward()
+    nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+    optimizer.step()
+    scheduler.step()
+    optimizer.zero_grad()
+
+  return correct_predictions.double() / n_examples, torch.mean(torch.tensor(losses))
+
+def eval_model(model, data_loader, loss_fn, device, n_examples):
+  model = model.eval()
+  losses = []
+  correct_predictions = 0
+  preds = []
+
+  with torch.no_grad():
+    for d in data_loader:
+      input_ids = d["input_ids"].to(device)
+      attention_mask = d["attention_mask"].to(device)
+      emotions = d["emotions"].to(device)
+      outputs = model(
+        input_ids=input_ids,
+        attention_mask=attention_mask
+      )
+      _, preds = torch.max(outputs, dim=1)
+      loss = loss_fn(outputs, emotions)
+      correct_predictions += torch.sum(preds == emotions)
+      losses.append(loss.item())
+
+  return correct_predictions.double() / n_examples, torch.mean(torch.tensor(losses))
+
+import pandas as pd
+
+
+
+
+
+"""# BERT_model Class
+"""
+
+class Bert_model:
+  def __init__(self, model, encoder, tokenizer, maxlen):
+    self._model = model
+    self._encoder = encoder
+    self._tokenizer = tokenizer
+    self._device = "cpu"
+    self._maxlen = maxlen
+
+  def set_device(self, device):
+    self._device = device
+    self._model.to(device)
+
+  def _preprocess_text(self, text):
+      # eliminate URLs
+      result = re.result = re.sub(r"http\S+", "", text)
+      # eliminate email addresses
+      result = re.sub('\S*@\S*\s?', '', result)
+      return result
+
+  def _make_prediction_for_text(self, text):
+    text = self._preprocess_text(text)
+    encoding = self._tokenizer.encode_plus(
+        text,
+        add_special_tokens=True,
+        max_length=self._maxlen,
+        return_token_type_ids=False,
+        padding='max_length',
+        return_attention_mask=True,
+        return_tensors='pt'
+    )
+
+    input_ids = encoding['input_ids'].to(self._device)
+    attention_mask = encoding['attention_mask'].to(self._device)
+    outputs = self._model(
+        input_ids=input_ids,
+        attention_mask=attention_mask
+    )
+    outputs.to(self._device)
+    return outputs.cpu().detach().numpy()
+
+  def detect_emotions_with_labels(self, texts):
+    predicted_emotions = []
+    for text in texts:
+      emotion = self._make_prediction_for_text(text)
+      pred = np.argmax(emotion, axis=1)
+      prediction = self._encoder.inverse_transform(pred)
+      predicted_emotions.append(prediction[0])
+    return predicted_emotions
+
+  def detect_emotions_with_proba(self, texts):
+    predicted_emotions = []
+    for text in texts:
+      emotion_proba = self._make_prediction_for_text(text)
+      predicted_emotions.append(emotion_proba)
+    return np.concatenate(predicted_emotions)
+
+
+
+def get_model_metrics(model_name, texts, emotions):
+    PATH = ('/content/drive/MyDrive/Colab Notebooks/Emotion-Detection-From-Tweets-Using-BERT-and-SVM-Ensemble-Model-master/checkpoints/' +
+            model_name + '.bin')
+    model = EmotionsClassifier(len(emotions_list), model_names[model_name])
+    model.load_state_dict(torch.load(PATH))
+    model.eval()
+    tokenizer = get_tokenizer(model_names[model_name])
+    maxlen = maxlen_dict[model_names[model_name]]
+    bert_model = Bert_model(model, label_encoder, tokenizer, maxlen)
+    bert_model.set_device("cuda")
+    bert_predictions = bert_model.detect_emotions_with_labels(texts)
+    y_true = label_encoder.inverse_transform(emotions)
+    return compute_metrics(y_true, bert_predictions, model_name)
+
+
+
